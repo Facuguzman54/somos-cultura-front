d325c3de0832b39b1e6add6e5bef144390e8c1ed
Facundo <facundoguzman54@gmail.com>
2023-10-28 14:21:38 -0300

    nav y header con tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import { EventsSection } from '@/components/EventsSection'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        HOME
+      <EventsSection></EventsSection>
     </main>
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,45 @@
+"use client"
+import { useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
+
+export const Nav = () => {
+    const [open, setOpen] = useState(false)
+    const handleOpen = (e)=>{
+        setOpen(prev => !prev)
+    }
+    const links = [
+        { name: "Inicio", href: "/" },
+        { name: "Eventos", href: "/events" },
+        { name: "Nosotros", href: "/about" },
+    ]
+    return (
+        <nav className="shadow-md w-full fixed top-0 left-0">
+            <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+                <div className="font-bold text-2xl cursor-pointer flex items-center text-gray-800">
+                    <span>
+                        Somos Cultura
+                    </span>
+      
+                </div>
+                <div>
+                    <div className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden  ">
+                    {
+                        open ?
+                        <div className="flex h-full"><FontAwesomeIcon  className="w-4"  onClick={handleOpen} icon={faXmark} /> </div>
+                        :  <div className="flex h-full"><FontAwesomeIcon  className="w-4" onClick={handleOpen} icon={faBars}/> </div>  
+                    }
+                    </div>
+                    <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-400 ease-in ${open ? "top-[4.02rem]":"top-[-490px]"}`}>
+                        {links.map(link =>
+                            <li className="md:ml-8 text-xl text-gray-800 md:my-0 md-8"
+                                key={link.name}><a href={link.href}>{link.name}</a>
+                            </li>)}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
